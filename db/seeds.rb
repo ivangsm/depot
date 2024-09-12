39f99f4f2e0bb78821cc8b630b5c745efa5a3ac1
Iván Salazar <ivangio.salazar@gmail.com>
2024-09-12 01:26:20 -0600

    Product scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Product.delete_all
+
+Product.create!(title: "Harry Potter", description: "A magical book", image_url: "https://m.media-amazon.com/images/I/81NQA1BDlnL._SL1500_.jpg", price: 9.99)
