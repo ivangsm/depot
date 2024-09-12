a60fd6cd52299371290f71bef31c44e268ce2a7b
Iván Salazar <ivangio.salazar@gmail.com>
2024-09-12 02:01:56 -0600

    update title length validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   validates :title, :description, :image_url, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
+  validates :title, length: { minimum: 10, message: "title must be at least 10 characters" }
   validates :image_url, allow_blank: true, format: {
     with: /\.(gif|jpg|png)\Z/i,
     message: "must be a URL for GIF, JPG or PNG image."
