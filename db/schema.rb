6cf4cc35203d3730bd0042f26bc519f53dc91c08
Iván Salazar <ivangio.salazar@gmail.com>
2024-10-07 17:04:39 -0600

    scaffold cart and cart concern

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_12_060241) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_07_230034) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
