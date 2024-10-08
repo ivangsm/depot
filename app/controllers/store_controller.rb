45269d397d596d4e353d048805021487f3353d34
Iván Salazar <ivangio.salazar@gmail.com>
2024-10-08 00:09:34 -0600

    Create basic cart functionality

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    session[:counter] = (session[:counter] || 0) + 1
+    @count = session[:counter]
   end
 end
